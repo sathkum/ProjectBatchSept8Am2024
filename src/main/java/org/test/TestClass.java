f74da691d7b06b69590a55062cf7f6475617023f
sathkum <sathishkumar42067@gmail.com>
2024-09-11 13:49:24 +0530

    sathish's code

--- src/main/java/org/test/TestClass.java
+++ src/main/java/org/test/TestClass.java
@@ -3,6 +3,8 @@ package org.test;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("sathish");
+		System.out.println("kumar");
 	}
 
 }
